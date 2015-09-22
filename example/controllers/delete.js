0cdf361acd1ab51060c98ddd3b0dcf0f0c02d789
Jonathan Dunlap <jdunlap@outlook.com>
2015-09-21 18:05:57 -0700

    Swaggerd use [swagger.json] default added

--- example/controllers/delete.js
+++ example/controllers/delete.js
@@ -1,8 +1,8 @@
 /**
  * λ Delete user
  * →λ path DELETE /users/{userId}
- * →λ event.pathparam.userId {string} - Users ids
- * λ→ response {} 200 - User deleted31131
+ * →λ event.pathparam.userId {string} - Users id
+ * λ→ response {} 200 - User deleted
  */
 exports.handler = function(event, context) {
   var err = null, 
